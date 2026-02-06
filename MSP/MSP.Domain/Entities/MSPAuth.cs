19e38d984c36d1fdf0a9e791a9432c02e978f960
Vitor Gabriel Calazas Dalmolin <vitor.calazas@rrs-software.com.br>
2026-02-06 17:14:05 -0300

    https://github.com/vcalazas/MySuperApp/issues/7
      - inicio do módulo;

---
+++ MSP/MSP.Domain/Entities/MSPAuth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using static MSP.Domain.Helpers.Constants;
+
+namespace MSP.Domain.Entities
+{
+    public class MSPAuth : MSPBaseEntity
+    {
+        [Key]
+        public int AuthId { get; set; }
+
+        [ForeignKey("MSPPerson")]
+        public int PersonId { get; set; }
+        public AuthDeviceType DeviceType { get; set; }
+        public string? SO { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public string? Version { get; set; }
+    }
+}
---
+++ MSP/MSP.Domain/Helpers/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSP.Domain.Helpers
+{
+    public class Constants
+    {
+        public enum AuthDeviceType
+        {
+            Web = 1,
+            Mobile = 2,
+            Desktop = 3,
+        }
+    }
+}
